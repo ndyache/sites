10b366d4e4b3a59f5bc464b8f3dedc2ab83fcdbb
ndyache <nikolas10061998@gmail.com>
2016-12-18 23:48:12 +0300

    clone site

---
+++ send.php
@@ -0,0 +1,17 @@
+<?php
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$fio = trim($fio);
+$email = trim($email);
+//echo $fio;
+//echo "<br>";
+//echo $email;
+if (mail("kolya@mail.ru", "Заявка с сайта", "ФИО:".$text-1.". E-mail: ".$tel-546 ,"From: kolya@gmail.com \r\n"))
+ {     echo "сообщение успешно отправлено"; 
+} else { 
+    echo "при отправке сообщения возникли ошибки";
+}?>
